a98e0f94c0b712be41a00e05b973c73130a0d7f9
frodriguez <frpatino6@gmail.com>
2019-10-24 14:51:24 -0500

    HTTPS por HTTP

--- src/app/shared/home.service.ts
+++ src/app/shared/home.service.ts
@@ -14,7 +14,7 @@ export class HomeService {
     constructor(private http: HttpClient) {}
 
     private serverUrl = "http://190.24.154.2:446/solpe/GetLiberaSolpes/";
-    private serverDatabaseUrl = "https://190.24.154.2:446/solpe/";
+    private serverDatabaseUrl = "http://190.24.154.2:446/solpe/";
     /*private serverUrl = "http://192.168.0.6/solpeoracle/solpe/GetLiberaSolpes/";
     private serverDatabaseUrl = "http://192.168.0.6/solpeoracle/solpe/"*/
 
